e7605edcd7bdf9e52048184fc75686e645eef95b
Sean.Estey <estese@gmail.com>
2018-11-23 15:21:18 -0700

    Better logging for debugging

---
+++ mt4/wingman/deploy.sh
@@ -0,0 +1,4 @@
+cp Experts/wingman.mq4 ~/AppData/Roaming/MetaQuotes/Terminal/50CA3DFB510CC5A8F28B48D1BF2A5702/MQL4/Experts
+cp Indicators/StochRSI.mq4 ~/AppData/Roaming/MetaQuotes/Terminal/50CA3DFB510CC5A8F28B48D1BF2A5702/MQL4/Indicators
+cp Indicators/FisherTransform.mq4 ~/AppData/Roaming/MetaQuotes/Terminal/50CA3DFB510CC5A8F28B48D1BF2A5702/MQL4/Indicators
+cp Include/utility.mqh ~/AppData/Roaming/MetaQuotes/Terminal/50CA3DFB510CC5A8F28B48D1BF2A5702/MQL4/Include
